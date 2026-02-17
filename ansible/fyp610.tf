b889177081ebe3a898cf64a842596b7696de484c
Laura Sanchez <laura.sanchez@example.org>
2026-02-17 14:34:41 +0700

    Merge branch 'develop' into main

---
+++ ansible/fyp610.tf
@@ -0,0 +1,12 @@
+# kubernetes/deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: app-deployment
+spec:
+  replicas: 2
+  template:
+    spec:
+      containers:
+      - name: app
+        image: "gpievx:3.1.5"
